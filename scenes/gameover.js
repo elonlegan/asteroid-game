4bbc611ff0071ebe55ebd0669f832889f48fb73a
Angel Leon <elonlegan@gmail.com>
2021-04-28 11:30:17 -0500

    changed images

--- scenes/gameover.js
+++ scenes/gameover.js
@@ -1,19 +1,19 @@
-import { RestartButton } from '../components/restart-button.js';
+import { RestartButton } from "../components/restart-button.js";
 
 export class Gameover extends Phaser.Scene {
-	constructor() {
-		super({ key: 'gameover' });
-		this.restartButton = new RestartButton(this);
-	}
+  constructor() {
+    super({ key: "gameover" });
+    this.restartButton = new RestartButton(this);
+  }
 
-	preload() {
-		this.load.image('gameover', 'images/gameover.png');
-		this.restartButton.preload();
-	}
+  preload() {
+    this.load.image("gameover", "https://i.postimg.cc/RhxP9Ww6/gameover.png");
+    this.restartButton.preload();
+  }
 
-	create() {
-		this.add.image(500, 250, 'background');
-		this.restartButton.create();
-		this.gameoverImage = this.add.image(500, 90, 'gameover');
-	}
+  create() {
+    this.add.image(500, 250, "background");
+    this.restartButton.create();
+    this.gameoverImage = this.add.image(500, 90, "gameover");
+  }
 }
